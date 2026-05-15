9303ecf3773450dd49aa8be52aea135b2b4e1878
arkaitzsilva <arkaitz.develop@gmail.com>
2026-05-15 08:04:32 +0200

    Add mpv base16 theme

---
+++ modules/home-manager/pc/programs/mpv/settings/theme.nix
@@ -0,0 +1,40 @@
+{
+  flake.modules.homeManager.pc = { lib, config, ... }: {
+    programs.mpv = with config.scheme.withHashtag; {
+      config = {
+        background-color = base00;
+        osd-back-color = base01;
+        osd-border-color = base01;
+        osd-color = base05;
+        osd-shadow-color = base00;
+      };
+      scriptOpts = {
+        uosc.color = lib.concatMapAttrsStringSep "," (name: value: "${name}=${value}") {
+          background = base00;
+          background_text = base05;
+          foreground = base05;
+          foreground_text = base00;
+          uccess = base0B;
+          error = base08;
+        };
+        modernz = {
+          seekbarfg_color = base0D;
+          seekbarbg_color = base03;
+          seekbar_cache_color = base03;
+          window_title_color = base03;
+          window_controls_color = base03;
+
+          title_color = base05;
+          time_color = base05;
+          chapter_title_color = base05;
+          cache_info_color = base05;
+
+          middle_buttons_color = base0D;
+          side_buttons_color = base03;
+          playpause_color = base0D;
+          hover_effect_color = base0E;
+        };
+      };
+    };
+  };
+}
