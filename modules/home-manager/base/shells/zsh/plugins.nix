67227015a8305afcb41666592e5d4abdfe5a06f0
arkaitzsilva <arkaitz.develop@gmail.com>
2026-05-21 16:26:08 +0200

    Add oh-my-zsh plugins function

--- modules/home-manager/base/shells/zsh/plugins.nix
+++ modules/home-manager/base/shells/zsh/plugins.nix
@@ -1,11 +1,24 @@
 {
-  flake.modules.homeManager.base = { pkgs, ... }: {
+  flake.modules.homeManager.base = { pkgs, ... }:
+  let
+    ohMyZshPlugin = name: {
+      inherit name;
+      src = pkgs.fetchFromGitHub {
+        owner = "ohmyzsh";
+        repo = "ohmyzsh";
+        rev = "master";
+        hash = "sha256-A6HKwOEyd4jjbWbE5HjCVX9KgfG/jEHi7keSP5Ye/lg=";
+      };
+      file = "plugins/${name}/${name}.plugin.zsh";
+    };
+  in {
     programs.zsh.plugins = [
       {
         name = "fzf-tab";
         src = pkgs.zsh-fzf-tab;
         file = "share/fzf-tab/fzf-tab.plugin.zsh";
       }
+      (ohMyZshPlugin "git")
     ];
   };
 }
