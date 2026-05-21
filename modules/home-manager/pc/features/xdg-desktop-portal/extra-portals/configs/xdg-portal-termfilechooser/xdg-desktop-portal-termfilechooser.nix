d36e3669f329bcf698ad0e70b9364dec08d7011d
arkaitzsilva <arkaitz.develop@gmail.com>
2026-05-21 17:03:03 +0200

    remove spaces

--- modules/home-manager/pc/features/xdg-desktop-portal/extra-portals/configs/xdg-portal-termfilechooser/xdg-desktop-portal-termfilechooser.nix
+++ modules/home-manager/pc/features/xdg-desktop-portal/extra-portals/configs/xdg-portal-termfilechooser/xdg-desktop-portal-termfilechooser.nix
@@ -2,10 +2,10 @@
   flake.modules.homeManager.pc = { config, lib, ... }: lib.mkIf config.xdg.portal.enable {
     xdg.configFile."xdg-desktop-portal-termfilechooser/config".text = ''
       [filechooser]
-      create_help_file=1
-      cmd=${config.xdg.termfilechooser.wrapper}
-      default_dir=$XDG_DOWNLOAD_DIR
-      env=TERMCMD='${config.xdg.termfilechooser.term-cmd}'
+      create_help_file = 1
+      cmd = ${config.xdg.termfilechooser.wrapper}
+      default_dir = $XDG_DOWNLOAD_DIR
+      env=TERMCMD = '${config.xdg.termfilechooser.term-cmd}'
       open_mode = suggested
       save_mode = default
     '';
