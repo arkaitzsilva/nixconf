bc920e479fc6986f94c42155b564b0a8f86d5017
arkaitzsilva <arkaitz.develop@gmail.com>
2026-05-25 15:34:42 +0200

    Remove superfile

--- modules/home-manager/pc/programs/file-manager/superfile/settings/plugins.nix
+++
@@ -1,9 +0,0 @@
-{
-  flake.modules.homeManager.pc = { config, ... }: {
-    programs.superfile.settings = {
-      metadata = false;
-      enable_md5_checksum = false;
-      zoxide_support = config.programs.zoxide.enable;
-    };
-  };
-}
