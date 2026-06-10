c3e0b11994a6456d0b4fedae0f8a0414adc8f526
arkaitzsilva <arkaitz.develop@gmail.com>
2026-06-10 18:12:37 +0200

    Add qutebrowser cookies dump userscript

--- modules/home-manager/pc/programs/qt/qutebrowser/settings/binds.nix
+++ modules/home-manager/pc/programs/qt/qutebrowser/settings/binds.nix
@@ -2,7 +2,7 @@
   flake.modules.homeManager.pc = {
     programs.qutebrowser.keyBindings = {
       normal = {
-        "M" = "hint links spawn mpv {hint-url}";
+        "M" = "spawn --userscript export-cookies ;; hint links spawn mpv --ytdl-raw-options=cookies=\${XDG_RUNTIME_DIR}/cookies.txt {hint-url}";
       };
     };
   };
